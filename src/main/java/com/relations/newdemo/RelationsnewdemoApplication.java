6538b67d66c373fbd136a915c3050fdac23264ca
unknown <sahithipabba123@gmail.com>
2023-07-11 17:34:23 +0530

    first commit

---
+++ src/main/java/com/relations/newdemo/RelationsnewdemoApplication.java
@@ -0,0 +1,13 @@
+package com.relations.newdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsnewdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsnewdemoApplication.class, args);
+	}
+
+}
